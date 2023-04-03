a88752520de36c2020934d99e96b6616fea91e3e
oderomboya <davismboya3@gmail.com>
2023-04-03 07:55:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+  
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        appendToDom(object);
+        console.log(object);
+      })
+      .catch(function(error) {
+        let errorMessage = document.createElement('p');
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+        console.log(error.message);
+      });
+  }
+  
